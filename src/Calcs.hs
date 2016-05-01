ec5dc52717259bf482b36a52cde8ccea6814dd14
Ed Maphis <emaphis85@gmail.com>
2016-05-01 17:46:52 -0400

    Add and test convenience Gematria summing functions Add and test some work horse Gematria calculationf functions

--- src/Calcs.hs
+++ src/Calcs.hs
@@ -28,10 +28,56 @@ calcPS = calcGem psMap
 calcPX = calcGem pxMap
 calcBC str =  map (getNum bcMap) str
 
-
 -- > calcSE "balloon"
 --   [2,1,12,12,15,15,14]
 -- > calcPY "balloon"
 --   [2,1,3,3,6,6,5]
 -- > calcHE "balloon"
 --   [2,1,20,20,50,50,40]
+
+
+-- calculate the sum of Simple English, Simple Pythagorean, S Pythagorean,
+-- Pathagorean with Exceptions, Jewish, Bacon
+-- convenience function
+sumSE,sumPY,sumHE,sumPS,sumPX,sumBC :: String -> Int
+sumSE = sum . calcSE
+sumPY = sum . calcPY
+sumHE = sum . calcHE
+sumPS = sum . calcPS
+sumPX = sum . calcPX
+sumBC = sum . calcBC
+
+-- sumSE "balloon"
+-- 71
+-- sumPY "balloon"
+-- 26
+
+
+-- Some general purpose calculation functions
+-- take a list of words and produce a list of Word,Gematria tuples
+-- [String] -> Tuple
+
+-- calculate the Simple English, Pythagorean, Jewish geometria of a list of words
+calcGematria :: [String] -> [(String, Int, Int, Int)]
+calcGematria = map (\s -> (s, sumSE s, sumPY s, sumHE s))
+
+-- > calcGematria ["balloon"]
+--   [("balloon",71,26,183)]
+-- > calcGematria ["lets","go","on","a", "balloon", "ride"]
+--   [("lets",56,11,215),("go",22,13,57),("on",29,11,90),("a",1,1,1),("balloon",71,26,183),("ride",36,27,98)]
+
+
+
+-- calculate the Pythagorean Gematria of a list of words
+calcPythag :: [String] -> [(String, Int, Int, Int)]
+calcPythag = map (\s -> (s, sumPY s, sumPS s, sumPX s))
+
+-- calcPythag ["lets","go","on","a", "balloon", "ride"]
+-- [("lets",11,20,20),("go",13,13,13),("on",11,11,11),("a",1,1,1),("balloon",26,26,26),("ride",27,27,27)]
+-- calcPythag ["Im","king", "of", "all", "I", "survey"]
+-- [("Im",13,13,13),("king",23,23,32),("of",12,12,12),("all",7,7,7),("I",9,9,9),("survey",29,38,56)]
+
+
+-- calculate the Bacon Gematria of a list of words
+calcBacon :: [String] -> [(String, Int)]
+calcBacon = map (\s -> (s, sumBC s))
--- test/Calcs_Test.hs
+++ test/Calcs_Test.hs
@@ -6,7 +6,7 @@ import Tables
 import Calcs
 import Test.HUnit
 
--- test calcGem, calcSE
+-- test calcGem, calcXX, sumXX
 
 test_calcGem :: Test
 test_calcGem = TestCase $ do
@@ -36,7 +36,41 @@ test_calcXX = TestCase $ do
   assertEqual "test caclHE"
     [2,1,20,20,50,50,40] (calcHE "balloon")
 
+test_sumXX :: Test
+test_sumXX = TestCase $ do
+  assertEqual "test sumSE on balloon"
+    71 (sumSE "balloon")
+  assertEqual "2+1+12+15+15+14"
+    71 (2+1+12+12+15+15+14)
+  assertEqual "test sumPY"
+    26 (sumPY "balloon")
+
+test_calcGematria :: Test
+test_calcGematria = TestCase $ do
+  assertEqual "test calGem base case"
+    [] (calcGematria [])
+  assertEqual "test 'balloon'"
+    [("balloon",71,26,183)] (calcGematria ["balloon"])
+  assertEqual "lets go on a balloon ride"
+     [("lets",56,11,215),("go",22,13,57),("on",29,11,90),("a",1,1,1),("balloon",71,26,183),("ride",36,27,98)]
+     (calcGematria ["lets","go","on","a", "balloon", "ride"])
+
+
+test_calcPythag :: Test
+test_calcPythag = TestCase $ do
+  assertEqual "test calcPythag base case"
+    [] (calcPythag [])
+  assertEqual "test one word"
+    [("survey",29,38,56)] (calcPythag ["survey"])
+  assertEqual "lets go on a balloon ride"
+    [("lets",11,20,20),("go",13,13,13),("on",11,11,11),("a",1,1,1),("balloon",26,26,26),("ride",27,27,27)]
+    (calcPythag ["lets","go","on","a", "balloon", "ride"])
+  assertEqual "Im king of all I servey"
+    [("Im",13,13,13),("king",23,23,32),("of",12,12,12),("all",7,7,7),("I",9,9,9),("survey",29,38,56)]
+    (calcPythag ["Im","king", "of", "all", "I", "survey"])
+
 
 
 main :: IO Counts
-main = runTestTT $ TestList [test_calcGem, test_calcXX]
+main = runTestTT $ TestList
+  [test_calcGem, test_calcXX, test_sumXX, test_calcGematria, test_calcPythag]
