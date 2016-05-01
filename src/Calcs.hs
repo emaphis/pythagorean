3f0e1ed0d18ec022a006a15c2de5a6ae762e8055
Ed Maphis <emaphis85@gmail.com>
2016-05-01 10:34:52 -0400

    Add functions for calculating gematria

--- src/Calcs.hs
+++ src/Calcs.hs
@@ -5,13 +5,33 @@ module Calcs where
 
 import Tables
 import Data.Char (toUpper)
--- import qualified Data.Map as M -- temp
+
 
 -- calc gematria as a List of Int given a word and a GeMap
-calc :: GemMap -> String -> [Int]
-calc gMap str = map (getNum gMap . toUpper) str
+calcGem :: GemMap -> String -> [Int]
+calcGem gMap str = map (getNum gMap . toUpper) str
 
--- calc seMap "balloon"
+-- calcGem seMap "balloon"
 -- [2,1,12,12,15,15,14]
--- calc pyMap "balloon"
+-- calcGem pyMap "balloon"
 -- [2,1,3,3,6,6,5]
+
+
+-- calculate Simple English, Pythagorean, Jewish, Pythagorean S,
+-- Pythagorean Exceptions, Bacon
+calcSE,calcPY,calcHE,calcPS,calcPX,calcBC
+  :: String -> [Int]
+calcSE = calcGem seMap
+calcPY = calcGem pyMap
+calcHE = calcGem heMap
+calcPS = calcGem psMap
+calcPX = calcGem pxMap
+calcBC str =  map (getNum bcMap) str
+
+
+-- > calcSE "balloon"
+--   [2,1,12,12,15,15,14]
+-- > calcPY "balloon"
+--   [2,1,3,3,6,6,5]
+-- > calcHE "balloon"
+--   [2,1,20,20,50,50,40]
